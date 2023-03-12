2f1e8ad67000f78a266b2ea91e3a3afb6d7ca699
o_w <womarow@gmail.com>
2023-03-12 08:49:36 +0300

    Microservicing AI models ci/cd concept

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="omarow/omarproject"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag omarow/omarproject $dockerpath
+echo "Docker ID Image: $dockerimage"
 
 # Step 3:
 # Push image to a docker repository
+docker push omarow/omarproject
